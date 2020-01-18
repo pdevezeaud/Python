c91ba6cd115d4b2ee51d59336d3b3ff09936af63
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2020-01-18 21:06:36 +0100

    formation youtube

---
+++ COURS YOUTUBE/boucle.py
@@ -0,0 +1,20 @@
+
+
+compteur = 0
+while compteur < 10:
+    print ("Je suis une phrase")
+    compteur += 1
+
+print("Je suis sorti de la boucle.....")
+
+
+phrase = "bonjour tout le monde"
+for lettre in phrase:
+    print(lettre)
+
+print("a bientot")
+
+
+
+
+
