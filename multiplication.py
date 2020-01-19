115147d69278d042697fc6b5a410c364366188c5
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2020-01-19 17:05:39 +0100

    table de multiplication exo

---
+++ multiplication.py
@@ -0,0 +1,14 @@
+print("Table de Multiplication")
+# Afficher l'en-tête de la table composé des chiffres: 1 à 9
+
+for j in range(1, 10):
+   print('\t','|', j, end = '')
+print() # Aller vers une nouvelle ligne
+print("________________________________________________________________________________")
+
+# Affichage du corps de table
+for i in range(1, 10):
+    print('\n|',i, end =' ')
+
+
+    
