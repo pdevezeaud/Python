e41b42b69ad11ef4f14f6b1a1081fcb2d0d66f0d
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2020-02-09 14:10:20 +0100

    reprise du cours sur tuple

--- tuple.py
+++ tuple.py
@@ -1,7 +1,21 @@
-#creation du tuple (immuabilité (invariable)). Le tuple ne peut etre modififé ni etendu
-t = (1,2,3)
-print (len(t))
-print (t[0])
+t=(4,1,5,2,3,9,7,2,8)
+print(t)
+print(t[2:3])
+t +=(10,) #concenation
+print(t)
+print(len(t))
+ch ='trou du cul'
+st = tuple(ch)
+print(st)
+print("*******************************************")
 
-# methode
-print(t.index(2))
+'''
+(!) Tuple : conteneur imuable (dont on ne peut modifier les valeurs)
+création de tupe : mon_tuple = ()      #vide
+                   mon_tuple = 17      #avec une valeurs
+                   mon_tuple = (4,17)  #plusieurs valeurs
+
+raisons d'utiliser les tuples : affectation multiple de variable
+                                retour multiple de fonction
+
+'''
