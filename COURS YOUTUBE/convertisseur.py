a89267652a86cc8277d12323e75f4c99312c8003
Philippe <pdevezeaud@yahoo.fr>
2020-02-27 13:27:00 +0100

    suivi de l'exercie de conversion

--- COURS YOUTUBE/convertisseur.py
+++ COURS YOUTUBE/convertisseur.py
@@ -1,6 +1,24 @@
 import math 
-def radDeg(deg):
-    calcul = (deg * 3.14) /180
+
+
+def degRad(deg):
+    degnumber = input("Entrez le nombre pour la conversion : ")
+    calcul = (deg * math.pi) /180
     return print(calcul)
 
-radDeg(1)  
+
+def radDeg(rad):
+    radnumber = input("Entrez le nombre pour la conversion : ")
+    calcul = (rad*180) / math.pi
+    return print(calcul)
+
+print ("Veuillez choisir le type de calcul")
+print("1 : conversion Radian vers Degré")
+print("2 : Conversion Degré vers Radian")
+print("Q : Quitter le programme")
+print(" ")
+print("* * * * * * * * * * * * ")
+#choix = input("Veuillez entrer votre choix ? :")
+
+#degRad(1)
+#radDeg(1)  
