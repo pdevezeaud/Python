9014a1ebbd3d17f8f7ce41e426cdfe77d6d78fac
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2020-03-08 15:14:33 +0100

    Code finalisé et corrigé

--- COURS YOUTUBE/convertisseur.py
+++ COURS YOUTUBE/convertisseur.py
@@ -9,30 +9,37 @@ def radDeg(rad):
     calcul = (rad*180) / math.pi
     return calcul
 
+print("Convertisseur Degré en Radian et de Radian en degré")
+print()
 print ("Veuillez choisir le type de calcul")
-print("1 : conversion Radian vers Degré")
-print("2 : Conversion Degré vers Radian")
-print("Q : Quitter le programme")
-print(" ")
+print("(1) : conversion Radian vers Degré")
+print("(2) : Conversion Degré vers Radian")
+print("(3) : Quitter le programme")
+print()
 print("* * * * * * * * * * * * ")
-choix = input("Veuillez entrer votre choix ? :")
 
+loop = 1
+while loop == 1:
 
-while choix != "Q":
+    choix = float(input("Veuillez entrer votre choix ? :"))
 
-    if choix == "1":
-        degnumber = float(input("Entrez le nombre pour la conversion : "))
+    if choix == 1:
+        degnumber = float(input("Entrez le nombre de Radian pour la conversion en Degré : "))
+        print("Résultat de la conversion Radian vers Degré")
         print(degRad(degnumber))
         
 
-    elif choix == "2":
-        radnumber = float(input("Entrez le nombre pour la conversion : "))
+    elif choix == 2:
+        radnumber = float(input("Entrez le nombre de Dégré pour la conversion en Radian : "))
+        print("Résultat de la conversion Degré vers Radian")
         print(radDeg(radnumber))
-        
-        
+               
     
-    else choix == "Q":
-        print ("fin de programme")
+    elif choix == 3:
+        loop = 0
+        print()
+        print ("FIN DU PROGRAMME")
+        print()
     
 
 
